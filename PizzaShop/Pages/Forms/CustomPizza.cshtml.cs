24df8852b13fbc91274730895de8bb9981026376
Richard Chalk <barmybrit@gmail.com>
2022-07-01 15:48:11 +0200

    Added logic for pricing ingredients

--- PizzaShop/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzaShop/Pages/Forms/CustomPizza.cshtml.cs
@@ -14,9 +14,21 @@ namespace PizzaShop.Pages.Forms
         {
         }
 
-        //public IActionResult OnPost()
-        //{
+        public IActionResult OnPost()
+        {
+            Pizza.Price = Pizza.BasePrice;
 
-        //}
+            //Extra cost for ingredients
+            if (Pizza.TomatoSauce) PizzaPrice += 20;
+            if (Pizza.Beef) PizzaPrice += 40;
+            if (Pizza.Cheese) PizzaPrice += 10;
+            if (Pizza.Ham) PizzaPrice += 20;
+            if (Pizza.Mushroom) PizzaPrice += 10;
+            if (Pizza.Peperoni) PizzaPrice += 30;
+            if (Pizza.Pineapple) PizzaPrice += 20;
+            if (Pizza.Tuna) PizzaPrice += 40;
+
+            return RedirectToPage("/Checkout/Checkout", new {Pizza.PizzaName, Pizza.Price });
+        }
     }
 }
